3409224aad0b9a69d11dbe574c62e407f8f7c3de
Hugo Landim Santos <hugo.landim.santos@accenture.com>
2019-03-02 00:57:49 -0300

    Adding all movie info at screen

--- app/src/main/java/com/arctouch/codechallenge/viewmodel/MovieDetailsViewModel.kt
+++ app/src/main/java/com/arctouch/codechallenge/viewmodel/MovieDetailsViewModel.kt
@@ -4,8 +4,12 @@ import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import com.arctouch.codechallenge.model.Movie
 
-class MovieDetailsViewModel : ViewModel(){
+class MovieDetailsViewModel : ViewModel() {
 
     var movie: MutableLiveData<Movie> = MutableLiveData()
+
+    fun getGenresString(): String? {
+        return movie.value?.genres?.joinToString(separator = ", ") { it.name }
+    }
 
 }
