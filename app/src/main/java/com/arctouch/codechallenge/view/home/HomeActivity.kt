091ce585a9233e825339ea997586fe6be0dd9d6e
Hugo Landim Santos <hugo.landim.santos@accenture.com>
2019-03-03 02:43:31 -0300

    Removing test code

--- app/src/main/java/com/arctouch/codechallenge/view/home/HomeActivity.kt
+++ app/src/main/java/com/arctouch/codechallenge/view/home/HomeActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.RecyclerView
-import android.widget.Toast
 import com.arctouch.codechallenge.R
 import com.arctouch.codechallenge.databinding.HomeActivityBinding
 import com.arctouch.codechallenge.model.Movie
@@ -34,8 +33,6 @@ class HomeActivity : AppCompatActivity(), HomeAdapter.ListListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val movieService = MovieService(TmdbApi.create())
-        val movieRepository = MovieRepository(movieService)
 
         binding = DataBindingUtil.setContentView(this, R.layout.home_activity)
         binding.lifecycleOwner = this
@@ -43,26 +40,18 @@ class HomeActivity : AppCompatActivity(), HomeAdapter.ListListener {
         val adapter = HomeAdapter(emptyList<Movie>().toMutableList())
         adapter.listener = this
         binding.recyclerView.adapter = adapter
-        val viewModel: MoviesViewModel by lazy {
-            getViewModel { MoviesViewModel(movieRepository) }
-        }
+
+
+        val viewModel = createViewModel()
+
         this.lifecycle.addObserver(viewModel)
         binding.viewModel = viewModel
-        viewModel.communicationErro.observe(this, Observer { message ->
-            Snackbar.make(binding.rootLayout, message.toString(), Snackbar.LENGTH_LONG).show()
-        })
-
-        binding.viewModel!!.isLoading.observe(this, Observer { isModelViewLoading ->
-            this.isLoading = isModelViewLoading!!
-        })
-
         binding.recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
             override fun onScrolled(recyclerView: RecyclerView?, dx: Int, dy: Int) {
                 super.onScrolled(recyclerView, dx, dy)
 
                 if (!recyclerView!!.canScrollVertically(1)
                         && !isLoading) {
-                    test()
                     binding.viewModel!!.requestNextMoviePage()
 
                 }
@@ -70,10 +59,25 @@ class HomeActivity : AppCompatActivity(), HomeAdapter.ListListener {
         })
     }
 
-    fun test() {
-        Toast.makeText(this, "Last", Toast.LENGTH_SHORT).show()
-    }
+    private fun createViewModel(): MoviesViewModel {
 
+        val movieService = MovieService(TmdbApi.create())
+        val movieRepository = MovieRepository(movieService)
+
+        val viewModel: MoviesViewModel by lazy {
+            getViewModel { MoviesViewModel(movieRepository) }
+        }
+
+        viewModel.communicationErro.observe(this, Observer { message ->
+            Snackbar.make(binding.rootLayout, message.toString(), Snackbar.LENGTH_LONG).show()
+        })
+
+        viewModel.isLoading.observe(this, Observer { isModelViewLoading ->
+            this.isLoading = isModelViewLoading!!
+        })
+
+        return viewModel
+    }
 
     override fun onRowClicked(movie: Movie) {
         val detailsFragment = MovieDetailsFragment.newInstance(movie)
