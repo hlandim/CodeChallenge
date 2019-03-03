58f37793e781eba7921d96760b9396f2fb0077ee
Hugo Landim Santos <hugo.landim.santos@accenture.com>
2019-03-03 14:04:01 -0300

    Layout adjusts

--- app/src/main/java/com/arctouch/codechallenge/viewmodel/MovieDetailsViewModel.kt
+++ app/src/main/java/com/arctouch/codechallenge/viewmodel/MovieDetailsViewModel.kt
@@ -1,15 +1,18 @@
 package com.arctouch.codechallenge.viewmodel
 
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import com.arctouch.codechallenge.R
 import com.arctouch.codechallenge.model.Movie
 
-class MovieDetailsViewModel : ViewModel() {
+class MovieDetailsViewModel(application: Application) : AndroidViewModel(application) {
 
     var movie: MutableLiveData<Movie> = MutableLiveData()
 
-    fun getGenresString(): String? {
-        return movie.value?.genres?.joinToString(separator = ", ") { it.name }
-    }
+    fun getGenresString() = movie.value?.genres?.joinToString(separator = ", ") { it.name }
+
+    fun getReleasedDate() = getApplication<Application>().getString(R.string.released_date) + movie.value?.releaseDate
 
 }
