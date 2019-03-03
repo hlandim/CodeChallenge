977417ee24152ccf3adbeef1cc2beeaff11fdaaf
Hugo Landim Santos <hugo.landim.santos@accenture.com>
2019-03-03 02:58:22 -0300

    Adding Search view

--- app/src/main/java/com/arctouch/codechallenge/view/home/HomeActivity.kt
+++ app/src/main/java/com/arctouch/codechallenge/view/home/HomeActivity.kt
@@ -1,11 +1,17 @@
 package com.arctouch.codechallenge.view.home
 
+import android.app.SearchManager
 import android.arch.lifecycle.Observer
+import android.content.Context
+import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.SearchView
+import android.view.Menu
+import android.widget.Toast
 import com.arctouch.codechallenge.R
 import com.arctouch.codechallenge.databinding.HomeActivityBinding
 import com.arctouch.codechallenge.model.Movie
@@ -57,6 +63,35 @@ class HomeActivity : AppCompatActivity(), HomeAdapter.ListListener {
                 }
             }
         })
+
+        handleIntent(intent)
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        if (intent != null) {
+            handleIntent(intent)
+        }
+    }
+
+    private fun handleIntent(intent: Intent) {
+
+        if (Intent.ACTION_SEARCH == intent.action) {
+            val query = intent.getStringExtra(SearchManager.QUERY)
+            Toast.makeText(this, query, Toast.LENGTH_SHORT).show()
+            //use the query to search your data somehow
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+// Associate searchable configuration with the SearchView
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        (menu.findItem(R.id.search).actionView as SearchView).apply {
+            setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        }
+
+        return true
     }
 
     private fun createViewModel(): MoviesViewModel {
